c76cf2bfd923a066b9a9424e538b407b61631a42
Neo <arttneo@gmail.com>
2014-08-24 12:27:50 +0300

    сотворил раскропитель (ad.php) обьявы (ad.txt) на части (titl, text, name etc.) Вроде работает :)

---
+++ ad.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+  <head>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" charset="utf-8">
+    <title>AD</title>
+  </head>
+
+<?php
+	
+		$ss = file_get_contents('ad.txt');
+		$a = array('titl','text','phon','name','mail');
+		$aa = array();
+
+$x=-1;
+while ($x++<10) {
+	if ($x==5) break;							//отрезаем от текста лишнее по меткам
+	$t = substr($ss, strpos($ss, $a[$x])+4); 	//в начале
+	$t = substr($t, 0, strpos($t, $a[$x]));		//в конце
+	$aa[]=$t;									//нарезанные куски в массив $aa
+	$t='';
+				}
+
+print_r($aa);
+
+?>
--- index.php
+++ index.php
@@ -1,2 +1,32 @@
-<?php
+<!DOCTYPE html>
+  <head>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" charset="utf-8">
+    <title>AD</title>
+  </head>
 
+
+<?php
+	
+
+
+
+
+$url = "http://www.0432.ua/doska/add";
+
+	$s = file_get_contents($url);
+	$s = str_replace(
+		'<input type="text" name="name" id="name" value="', 
+		'<input type="text" name="name" id="name" value="',$s);
+	
+	print $s;
+
+//$captcha = substr($s, strpos($s, '/tmp/captcha/'));
+
+//$captcha = substr($captcha, 0, strpos($captcha, '"'));
+
+		
+
+		//print "<img src='http://www.0432.ua$captcha'>";
+
+
+?>
